4a41e0efa9cd217b6d508e131c2cc4141f5befb1
Yosi Amiga <yosiamiga1710@gmail.com>
2023-03-28 09:20:03 +0300

    Added Client side face detection using face-api.js

--- src/components/Camera.js
+++ src/components/Camera.js
@@ -1,10 +1,101 @@
 import React, { useState, useEffect, useRef } from 'react';
+import * as faceapi from 'face-api.js';
 
-const Camera = ({ onCapture, userEmail, }) => {
+/**
+ * Camera component that allows the user to take pictures using the device's camera.
+ *
+ * @param {Object} props - Component props.
+ * @param {function} props.onCapture - Callback function to be called when a picture is taken.
+ * @param {string} props.userEmail - Email of the logged in user.
+ * @param {function} props.onAutoCapture - Callback function to be called when a picture is automatically taken by the camera.
+ * @returns {JSX.Element} - Camera component.
+ */
+const Camera = ({ userEmail, onAutoCapture }) => {
   const videoRef = useRef(null);
   const [stream, setStream] = useState(null);
   const [cameraActive, setCameraActive] = useState(false);
+  const faceDetectionTimeout = useRef(null); // Add this line
 
+  /**
+   * Load the face detection models from the face-api.js library when the component mounts.
+   */
+  useEffect(() => {
+    const loadModels = async () => {
+      await faceapi.nets.ssdMobilenetv1.loadFromUri('/models');
+      //For more features:
+      //await faceapi.nets.faceLandmark68Net.loadFromUri('/models');
+      //await faceapi.nets.faceExpressionNet.loadFromUri('/models');
+    };
+
+    loadModels();
+  }, []);
+
+  /**
+   * Detect faces in the camera stream using the face-api.js library and draw the detection results on a canvas.
+   */
+  const detectFace = async () => {
+    if (!videoRef.current || !stream) return;
+
+    const detections = await faceapi.detectSingleFace(videoRef.current);
+    if (detections && videoRef.current) {
+      const existingCanvas = document.getElementById('overlay');
+      if (existingCanvas) existingCanvas.remove();
+
+      const canvas = faceapi.createCanvasFromMedia(videoRef.current);
+      canvas.id = 'overlay';
+      canvas.className = 'overlay'; // Add this line to apply the CSS class
+      videoRef.current.parentElement.appendChild(canvas);
+      const displaySize = {
+        width: videoRef.current.videoWidth,
+        height: videoRef.current.videoHeight,
+      };
+      faceapi.matchDimensions(canvas, displaySize);
+      const resizedDetections = faceapi.resizeResults(detections, displaySize);
+      faceapi.draw.drawDetections(canvas, resizedDetections);
+      //For more features:
+      //faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+      //faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+      // Capture the screenshot when the detection score is above 0.9
+      if (detections.score >= 0.99) {
+        handleAutoCapture();
+      }
+    }
+  };
+
+  /**
+   * Start the face detection interval when the component mounts and stop it when the component unmounts or the camera stream is disabled.
+   */
+  useEffect(() => {
+    if (stream) {
+      const interval = setInterval(detectFace, 100);
+      return () => {
+        clearInterval(interval);
+      };
+    }
+  }, [stream]);
+
+  /**
+   * Automatically capture a picture from the camera and call the `onAutoCapture` callback.
+   */
+  const handleAutoCapture = () => {
+    if (faceDetectionTimeout.current) clearTimeout(faceDetectionTimeout.current);
+
+    const canvas = document.createElement('canvas');
+    canvas.width = videoRef.current.videoWidth;
+    canvas.height = videoRef.current.videoHeight;
+    const context = canvas.getContext('2d');
+    context.drawImage(videoRef.current, 0, 0, canvas.width, canvas.height);
+    canvas.toBlob((blob) => {
+      const timestamp = new Date().toLocaleString('en-US').replace(/[/,:]/g, '-');
+      const fileName = `${userEmail}_${timestamp}.jpeg`;
+      onAutoCapture(new File([blob], fileName, { type: 'image/jpeg' }));
+      disableStream();
+    }, 'image/jpeg', 0.95);
+  };
+  
+  /**
+   * Enables or disables the camera stream based on the `cameraActive` state.
+   */
   useEffect(() => {
     if (cameraActive) {
       enableStream();
@@ -19,6 +110,10 @@ const Camera = ({ onCapture, userEmail, }) => {
     };
   }, [cameraActive]);
 
+  /**
+   * @description Enables the camera stream by requesting access to the user's camera and setting the stream to the video element.
+   * @returns {Promise<void>}
+   */
   const enableStream = async () => {
     try {
       const stream = await navigator.mediaDevices.getUserMedia({ video: true });
@@ -29,6 +124,10 @@ const Camera = ({ onCapture, userEmail, }) => {
     }
   };
 
+  /**
+   * @description Disables the camera stream by stopping all tracks and setting the `stream` state to `null`.
+   * @returns {void}
+   */
   const disableStream = () => {
     if (stream) {
       stream.getTracks().forEach(track => track.stop());
@@ -36,31 +135,16 @@ const Camera = ({ onCapture, userEmail, }) => {
     }
   };
 
-  const handleCapture = () => {
-    const canvas = document.createElement('canvas');
-    canvas.width = videoRef.current.videoWidth;
-    canvas.height = videoRef.current.videoHeight;
-    const context = canvas.getContext('2d');
-    context.drawImage(videoRef.current, 0, 0, canvas.width, canvas.height);
-    canvas.toBlob((blob) => {
-      const timestamp = new Date().toLocaleString('en-US').replace(/[/,:]/g, '-');
-      const fileName = `${userEmail}_${timestamp}.jpeg`;
-      onCapture(new File([blob], fileName, { type: 'image/jpeg' }));
-    }, 'image/jpeg', 0.95);
-  };
-
   return (
     <div style={{ textAlign: 'center' }}>
-      {cameraActive && <video ref={videoRef} autoPlay playsInline />}
-      <div>
-        <button onClick={() => setCameraActive(!cameraActive)} style={{ marginRight: '8px', marginTop: '8px' }}>
-          {cameraActive ? 'Close Camera' : 'Open Camera'}
+    {cameraActive && 
+          <div className="video-container">
+          <video ref={videoRef} autoPlay playsInline />
+        </div>}
+    <div>
+      <button onClick={() => setCameraActive(!cameraActive)} style={{ marginRight: '8px', marginTop: '8px' }}>
+        {cameraActive? 'Close Camera' : 'Open Camera'}
         </button>
-        {cameraActive && (
-          <button onClick={handleCapture} style={{ marginLeft: '8px', marginTop: '8px' }}>
-            Capture
-          </button>
-        )}
       </div>
     </div>
   );
--- src/components/ImageUpload.js
+++ src/components/ImageUpload.js
@@ -2,19 +2,39 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import Camera from './Camera';
 
+/**
+ * Renders a component for uploading images. Allows the user to select an image file or capture an image with their camera.
+ * @param {Function} onLogout - Callback function to handle user logout.
+ * @param {string} userEmail - The email address of the currently logged in user.
+ * @returns {JSX.Element} - A JSX element that renders an image preview, a camera component, and a form to upload the selected file.
+ */
 const ImageUpload = ({ onLogout, userEmail }) => {
   const [selectedFile, setSelectedFile] = useState(null);
   const [showCamera, setShowCamera] = useState(true);
 
+  /**
+   * Sets the selected file to the captured image blob and hides the camera component.
+   * @param {Blob} blob - The blob of the captured image.
+   * @returns {void}
+   */
   const handleCapture = (blob) => {
     setSelectedFile(blob);
     setShowCamera(false);
   };
 
+  /**
+   * Removes the selected file from state to allow the user to retake the image.
+   * @returns {void}
+   */
   const handleRetake = () => {
     setSelectedFile(null);
   };
 
+  /**
+   * Submits the selected file to the server for upload.
+   * @param {Event} event - The form submission event.
+   * @returns {Promise<void>}
+   */
   const handleSubmit = async (event) => {
     event.preventDefault();
     const formData = new FormData();
@@ -40,7 +60,7 @@ const ImageUpload = ({ onLogout, userEmail }) => {
           </div>
         </>
       ) : (
-        <Camera onCapture={handleCapture} userEmail={userEmail} />
+        <Camera onAutoCapture={handleCapture} userEmail={userEmail} />
       )}
       <form className='register-form' onSubmit={handleSubmit}>
         <input type="file" accept="image/*" onChange={(event) => setSelectedFile(event.target.files[0]) } />
