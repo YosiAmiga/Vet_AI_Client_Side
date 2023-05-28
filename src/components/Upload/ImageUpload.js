fbc10969ed8110e8d3c08b7e95c8564b8a61715f
Yosi Amiga <yosiamiga1710@gmail.com>
2023-05-28 12:15:01 +0300

    Adding Hamster loader

--- src/components/Upload/ImageUpload.js
+++ src/components/Upload/ImageUpload.js
@@ -82,9 +82,27 @@ const ImageUpload = ({ onLogout, userEmail}) => {
 
   return (
     <div style={{ textAlign: 'center' }}>
-      <h2>Upload Image</h2>
+      <h2>Upload File</h2>
       {loading ? (
-        <BarLoader color={'#123abc'} loading={loading} size={150} />
+                <div className="wheel-and-hamster">
+                <div className="wheel"></div>
+                <div className="hamster">
+                  <div className="hamster__head">
+                    <div className="hamster__ear"></div>
+                    <div className="hamster__ear"></div>
+                    <div className="hamster__eye"></div>
+                    <div className="hamster__nose"></div>
+                  </div>
+                  <div className="hamster__body">
+                    <div className="hamster__limb--fr"></div>
+                    <div className="hamster__limb--fl"></div>
+                    <div className="hamster__limb--br"></div>
+                    <div className="hamster__limb--bl"></div>
+                    <div className="hamster__tail"></div>
+                  </div>
+                </div>
+                <div className="spoke"></div>
+              </div>
       ) : (
         <>
           {selectedFile ? (
